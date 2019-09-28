a47699ac582657c31216ddc3f37ef89fdc38a6c7
noituri <yknomeh@protonmail.com>
2019-09-28 20:15:31 +0200

    pages: landing page

--- layouts/default.js
+++ layouts/default.js
@@ -100,6 +100,97 @@ const Default = ({ title, children }) => {
                 /* color: #000; */
                 background-color: rgb(255, 2, 44);
             }
+            
+            @media only screen and (min-width: 750px) {
+                .logo .name {
+                    margin-top: .4rem;
+                }
+                .content {
+                    padding: 1rem 7rem 1rem !important;
+                }
+                .dashboard {
+                    margin-left: 0.6rem !important;
+                    margin-right: 0.6rem !important;
+                }
+                .dashboard .content {
+                    padding: 1rem !important;
+                }
+                .activities+.deploy-panel {
+                    display: block !important;
+                }
+                .deploy-panel {
+                    border-left: 2px solid rgba(255, 255, 255, 0.9) !important;
+                    margin-top: -2.5rem !important;
+                    padding-top: 2rem;
+                    padding-left: 1rem !important;
+                    margin-right: 1rem !important;
+                }
+            }
+            
+            @media only screen and (min-width: 1000px) {
+                .logo .name {
+                    margin-top: 0rem;
+                }
+                .content {
+                    padding: 1rem 15rem 1rem !important;
+                }
+                .dashboard {
+                    margin-left: 2rem !important;
+                    margin-right: 2rem !important;
+                }
+                .dashboard .content {
+                    padding: 1rem !important;
+                }
+                .title {
+                    font-size: 4rem !important;
+                }
+                nav {
+                    font-size: 1.5rem !important;
+                }
+                .button .big {
+                    max-width: 10rem !important;
+                    font-size: 2rem !important;
+                }
+                .register .button, .login .button {
+                    max-width: 15rem !important;
+                    font-size: 1.5rem !important;
+                }
+                .app-selector {
+                    grid-template-columns: auto auto auto !important;
+                }
+            }
+            
+            @media only screen and (min-width: 1300px) {
+                .home {
+                    margin-top: 5%;
+                    /* float: left; */
+                }
+                .home>.title {
+                    margin-left: auto;
+                    margin-right: auto;
+                    max-width: 30rem;
+                }
+                .app .app-logo {
+                    width: 7rem !important;
+                    height: 7rem !important;
+                    font-size: 4rem !important;
+                    line-height: 7rem !important;
+                }
+            }
+            
+            @media only screen and (min-width: 1900px) {
+                .dashboard {
+                    margin-left: 30rem !important;
+                    margin-right: 30rem !important;
+                }
+            }
+            
+            @media only screen and (min-width: 2200px) {
+                .dashboard {
+                    margin-left: 40rem !important;
+                    margin-right: 40rem !important;
+                }
+            }
          `}</style>
 
          <title>Radix - {title}</title>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Layout from '../layouts/default'
 
 const Index = () => (
-  <Layout title="Home">
-    <p>Hello Next.js</p>
-  </Layout>
+   <Layout title="Home">
+      <style jsx>{`
+         .home {
+            text-align: center;
+         }
+     `}</style>
+      <div className="content">
+         <article className="home">
+            <h2 className="title">
+               <span className="accent">Host</span> your bots <span className="accent">with ease</span>
+            </h2>
+            <p className="button big" style={{marginTop: '3rem', padding: '1rem 2rem 1rem'}}>
+               Try it now!
+            </p>
+         </article>
+      </div>
+   </Layout>
 );
 
 export default Index;
