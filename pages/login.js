ee282a87c53f6ab06ea698bc39c60eecacbcdf8f
noituri <yknomeh@protonmail.com>
2019-09-29 19:44:23 +0200

    pages: sigin in work(?)

--- pages/login.js
+++ pages/login.js
@@ -1,35 +1,86 @@
 import React from 'react'
 import Link from 'next/link'
+import axios from 'axios'
 import Layout from '../layouts/default'
 import ErrorAlert from '../components/ErrorAlert'
+import { useRouter } from 'next/router'
+import { LOGIN_ENDPOINT } from '../constants'
 
-const Login = () => {
-   return (
-      <Layout title="Sign in">
-         <div className="content">
-            <article className="login">
-               <h3 className="title bold">
-                  Sing In
-               </h3>
-               <div className="form">
-                  <ErrorAlert errorMessage="" />
-                  <input placeholder="Email address" type="text"/>
-                  <br/>
-                  <input className="form.password" placeholder="Password" type="password"/>
-                  <br/>
-                  <p className="button" style={{marginTop: '2rem', padding: '1rem 2rem 1rem'}}>
-                     Sign In
-                  </p>
-                  <Link href="/register">
-                     <p style={{marginTop: '1rem'}}>
-                        Don't have an account? <span className="accent" style={{textDecoration: 'none', cursor: 'pointer'}}>Sign up!</span>
+class Login extends React.Component {
+   constructor(props){
+      super(props)
+      this.state = {
+         email: '',
+         pass: '',
+         errorMessage: ''
+      }
+
+      this.handleInputChange = this.handleInputChange.bind(this)
+      this.signIn = this.signIn.bind(this)
+   }
+
+   signIn(e) {
+      e.preventDefault()
+
+      const config = {
+         headers: {
+            Authorization: 'Basic ' + btoa(`${this.state.email}:${this.state.pass}`)
+         }
+      }
+
+      axios.post(LOGIN_ENDPOINT, {}, config)
+         .then(() => {
+            useRouter().push('/user/apps')
+         })
+         .catch((error) => {
+            const resp = error.response.data
+            if (resp.status === 'error') {
+               this.setState({
+                  errorMessage: 'resp.message'
+               })
+               return
+            }
+
+            this.setState({
+               errorMessage: 'Something went wrong. Try again later.'
+            })
+         })
+   }
+
+   handleInputChange(e) {
+      this.setState({
+         [e.target.name]: e.target.value
+      })
+   }
+
+   render() {
+      return (
+         <Layout title="Sign in">
+            <div className="content">
+               <article className="login">
+                  <h3 className="title bold">
+                     Sing In
+                  </h3>
+                  <div className="form">
+                     <ErrorAlert errorMessage={this.state.errorMessage} />
+                     <input name="email" placeholder="Email address" type="text" value={this.state.email} onChange={this.handleInputChange} />
+                     <br/>
+                     <input name="pass" className="form.password" placeholder="Password" type="password" value={this.state.pass} onChange={this.handleInputChange} />
+                     <br/>
+                     <p className="button" style={{marginTop: '2rem', padding: '1rem 2rem 1rem'}} onClick={this.signIn}>
+                        Sign In
                      </p>
-                  </Link>
-               </div>
-            </article>
-         </div>
-      </Layout>
-   )
+                     <Link href="/register">
+                        <p style={{marginTop: '1rem'}}>
+                           Don't have an account? <span className="accent" style={{textDecoration: 'none', cursor: 'pointer'}}>Sign up!</span>
+                        </p>
+                     </Link>
+                  </div>
+               </article>
+            </div>
+         </Layout>
+      )
+   }
 }
 
 export default Login
