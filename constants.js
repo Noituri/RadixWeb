6d66ab7ac20f958113f6e9d1c62c3a80469bf0a9
Mikolaj Radkowski <macbooc48@gmail.com>
2019-09-30 18:56:56 +0200

    pages: using react hooks in login

--- constants.js
+++ constants.js
@@ -1,2 +1,3 @@
 export const ADDRESS = 'http://localhost:4000'
 export const LOGIN_ENDPOINT = ADDRESS + '/api/auth/login'
+export const REGISTER_ENDPOINT = ADDRESS + '/api/auth/register'
--- pages/login.js
+++ pages/login.js
@@ -1,86 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import axios from 'axios'
 import Layout from '../layouts/default'
 import ErrorAlert from '../components/ErrorAlert'
 import { withRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { LOGIN_ENDPOINT } from '../constants'
 
-class Login extends React.Component {
-   constructor(props){
-      super(props)
-      this.state = {
-         email: '',
-         pass: '',
-         errorMessage: ''
-      }
+const Login = () => {
+   const [creds, setCreds] = useState({
+      email: '',
+      pass: '',
+      errorMessage: ''
+   })
+   const router = useRouter()
 
-      this.handleInputChange = this.handleInputChange.bind(this)
-      this.signIn = this.signIn.bind(this)
-   }
-
-   signIn(e) {
+   const signIn = (e) => {
       e.preventDefault()
 
       const config = {
          headers: {
-            Authorization: 'Basic ' + btoa(`${this.state.email}:${this.state.pass}`)
+            Authorization: 'Basic ' + btoa(`${creds.email}:${creds.pass}`)
          }
       }
 
       axios.post(LOGIN_ENDPOINT, {}, config)
-         .then((_resp) => {
-            this.props.router.push('/user/apps')
+         .then(() => {
+            router.push('/user/apps')
          })
          .catch((error) => {
             const resp = error.response
             if (resp !== undefined) {
-               this.setState({
+               setCreds({
+                  ...creds,
                   errorMessage: resp.data.message
                })
+
                return
             }
 
-            this.setState({
+            setCreds({
+               ...creds,
                errorMessage: 'Something went wrong. Try again later.'
             })
          })
    }
 
-   handleInputChange(e) {
-      this.setState({
+   const handleInputChange = (e) => {
+      setCreds({
+         ...creds,
          [e.target.name]: e.target.value
       })
    }
 
-   render() {
-      return (
-         <Layout title="Sign in">
-            <div className="content">
-               <article className="login">
-                  <h3 className="title bold">
-                     Sing In
-                  </h3>
-                  <div className="form">
-                     <ErrorAlert errorMessage={this.state.errorMessage} />
-                     <input name="email" placeholder="Email address" type="text" value={this.state.email} onChange={this.handleInputChange} />
-                     <br/>
-                     <input name="pass" className="form.password" placeholder="Password" type="password" value={this.state.pass} onChange={this.handleInputChange} />
-                     <br/>
-                     <p className="button" style={{marginTop: '2rem', padding: '1rem 2rem 1rem'}} onClick={this.signIn}>
-                        Sign In
+   return (
+      <Layout title="Sign in">
+         <div className="content">
+            <article className="login">
+               <h3 className="title bold">
+                  Sing In
+               </h3>
+               <div className="form">
+                  <ErrorAlert errorMessage={creds.errorMessage} />
+                  <input name="email" placeholder="Email address" type="text" value={creds.email} onChange={handleInputChange} />
+                  <br/>
+                  <input name="pass" className="form.password" placeholder="Password" type="password" value={creds.pass} onChange={handleInputChange} />
+                  <br/>
+                  <p className="button" style={{marginTop: '2rem', padding: '1rem 2rem 1rem'}} onClick={signIn}>
+                     Sign In
+                  </p>
+                  <Link href="/register">
+                     <p style={{marginTop: '1rem'}}>
+                        Don't have an account? <span className="accent" style={{textDecoration: 'none', cursor: 'pointer'}}>Sign up!</span>
                      </p>
-                     <Link href="/register">
-                        <p style={{marginTop: '1rem'}}>
-                           Don't have an account? <span className="accent" style={{textDecoration: 'none', cursor: 'pointer'}}>Sign up!</span>
-                        </p>
-                     </Link>
-                  </div>
-               </article>
-            </div>
-         </Layout>
-      )
-   }
+                  </Link>
+               </div>
+            </article>
+         </div>
+      </Layout>
+   )
 }
 
 export default withRouter(Login)
