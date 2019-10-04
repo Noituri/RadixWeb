d21d8df337984579dc77419f2ebde7645ab04853
noituri <yknomeh@protonmail.com>
2019-10-04 22:40:35 +0200

    pages: added dashboard

---
+++ pages/user/dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../../layouts/default'
+
+export default () => (
+   <Layout title="App not found">
+      <div className="content">
+         <h1 className="title">
+            App does not exist
+         </h1>
+      </div>
+   </Layout>
+)
